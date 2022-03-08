c62f57d57104187ba14724b81010b185f1eee07a
cat <20171607@cqu.edu.cn>
2022-03-08 09:41:11 +0800

    3

--- mv_normal.sh
+++ mv_normal.sh
@@ -1,4 +1,5 @@
-DATA_PATH="/mnt/d/wsl/data/cqu_bpdd"
+#DATA_PATH="/mnt/d/wsl/data/cqu_bpdd"
+DATA_PATH="/raid/Data/zhangyi/cqu_bpdd/"
 if [ $1 -eq 0 ]; then
     mv $DATA_PATH/test/normal $DATA_PATH/tenormal
     mv $DATA_PATH/train/normal $DATA_PATH/trnormal
